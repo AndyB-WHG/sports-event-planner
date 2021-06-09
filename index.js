a4db5dbc25124b12d3dcfc54e0fb4a9a87d49239
AndyB-WHG <andrew.bond2@gmail.com>
2021-06-09 07:06:19 +0000

    fix: amended Get request - still not working - but closer.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var xhr = new XMLHttpRequest();
 
-xhr.open("GET", 'https://api.schedjoules.com/pages/115673' -v -H 'Authorization: Token token="0443a55244bb2b6224fd48e0416f0d9c"');
+xhr.open("GET", "https://api.schedjoules.com/categories/");
+
+xhr.setRequestHeader("Authorization", "Bearer {0443a55244bb2b6224fd48e0416f0d9c}");
+
 xhr.send();
 
 xhr.onstatereadychange = function () {
