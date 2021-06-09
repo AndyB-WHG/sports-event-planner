7908aad829a96f28d01345503af68e615598343c
AndyB-WHG <andrew.bond2@gmail.com>
2021-06-09 20:15:50 +0000

    fix: switched to 'Sports Radar' API due to issues with SchedJoules API (unable to obtain Access Token).

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-var xhr = new XMLHttpRequest();
+// var xhr = new XMLHttpRequest();
 
-xhr.open("GET", "https://api.schedjoules.com/categories/");
+// xhr.open("GET", "https://api.schedjoules.com/categories/sports/boxing");
 
-xhr.setRequestHeader("Authorization", "Bearer {0443a55244bb2b6224fd48e0416f0d9c}");
+// xhr.setRequestHeader("Authorization", "Bearer {0443a55244bb2b6224fd48e0416f0d9c}");
 
-xhr.send();
+// xhr.send();
 
-xhr.onstatereadychange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
-    }
-};
+// xhr.onstatereadychange = function () {
+//     if (this.readyState == 4 && this.status == 200) {
+//         document.getElementById("data").innerHTML = this.responseText;
+//     }
+// };
+
+fetch("https://api.predicthq.com/v1/events/calendar?category=sports&country=GB", {
+  headers: {
+    Authorization: `Bearer 63gK8tsgkCkaRrApdNwOll0sbnXJzAD8EsFCJSSU`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
+
+  // curl -X GET -H "Authorization: Bearer 63gK8tsgkCkaRrApdNwOll0sbnXJzAD8EsFCJSSU" \
+  //           -H "Accept: application/json" \
