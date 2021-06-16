fa0e893ecac00c4fa07638a7197cd9a28b9877cd
AndyB-WHG <andrew.bond2@gmail.com>
2021-06-16 18:53:42 +0000

    fix: fixed the 'Autocomplete' function for the 'Quick Search' input box.

--- index.js
+++ index.js
@@ -13,24 +13,17 @@ fetch("https://api.predicthq.com/v1/places/?q=New+York&limit=5", {
         console.log(myContent.length);
         console.log("First item found is : " + myContent[0].name);
         for (i = 0, len = myContent.length, text="", options = []; i < len; i++) {
-            // options += "<option value=" + myContent[i].name + "></option>";
-            options[i] = myContent[i].name;
-            
             text += myContent[i].name + "<br>";
-
+            options.push(myContent[i].name);
             $(function() {
-                console.log("Starting 'Autocomplete' function");
-                console.log("Options are : " + options);
-                console.log(typeof options);
-                var availableTags = options;
+                var quickSearchList = options;
+                console.log("Options are: " + options)
                 $( "#quick-search-input-box" ).autocomplete({
-                  source: availableTags
+                  source: quickSearchList
                 });
               } );
         }
         document.getElementById("data").innerHTML = text;
-        console.log(options);
-        // document.getElementById("quick-search-input-box").innerHTML = options;
     });
 
 // $(document).ready(function() {
